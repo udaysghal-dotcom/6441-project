aa04bd96583347b2e3eb9cee38e3b99a917bb8e9
Uday Saihgal <z5507339@ad.unsw.edu.au>
2026-07-25 18:38:55 +1000

    added a few comments

--- tests/test_oaep.py
+++ tests/test_oaep.py
@@ -14,14 +14,12 @@ KEY_BITS = 1024
 def keypair():
     return generate_keypair(KEY_BITS)
 
-
 @pytest.mark.parametrize("length", [0, 1, 16, 62])
 def test_round_trip_varying_length(keypair, length):
     pub, priv = keypair
     msg = secrets.token_bytes(length)
     ct = encrypt(pub, msg)
     assert decrypt(priv, ct) == msg
-
 
 def test_encode_decode_directly():
     k = 128
@@ -30,7 +28,7 @@ def test_encode_decode_directly():
     assert len(encoded) == k
     assert oaep_decode(encoded, k) == msg
 
-
+# tamper detection by flipping bit in the middle of cipher text
 def test_ciphertext_bit_flip_rejected(keypair):
     pub, priv = keypair
     ct = bytearray(encrypt(pub, b"top secret"))
@@ -38,13 +36,11 @@ def test_ciphertext_bit_flip_rejected(keypair):
     with pytest.raises(OAEPError):
         decrypt(priv, bytes(ct))
 
-
 def test_wrong_label_rejected(keypair):
     pub, priv = keypair
     ct = encrypt(pub, b"labelled", label=b"A")
     with pytest.raises(OAEPError):
         decrypt(priv, ct, label=b"B")
-
 
 def test_message_too_long_rejected(keypair):
     pub, _ = keypair
@@ -52,10 +48,8 @@ def test_message_too_long_rejected(keypair):
     with pytest.raises(OAEPError):
         encrypt(pub, secrets.token_bytes(63))
 
-
 def _reference_key(pub, priv):
     return RSA.construct((pub.n, pub.e, priv.d, priv.p, priv.q))
-
 
 def test_cross_check_ours_encrypt_reference_decrypt(keypair):
     pub, priv = keypair
@@ -63,7 +57,6 @@ def test_cross_check_ours_encrypt_reference_decrypt(keypair):
     msg = b"hello 6441 tutor"
     ct = encrypt(pub, msg)
     assert ref.decrypt(ct) == msg
-
 
 def test_cross_check_reference_encrypt_ours_decrypt(keypair):
     pub, priv = keypair
